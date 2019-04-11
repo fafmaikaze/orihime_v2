18c9d05ea5695ba1735ed35b7c4eeba5cb283236
ASUS-maikaze <faf.maikaze@gmail.com>
2019-04-11 16:03:32 +0800

    PG這個見貨說沒FORMAT就COMMIT很雷

--- sqlite_helper.js
+++ sqlite_helper.js
@@ -9,50 +9,41 @@ var file = "./test.db";
 function initDB(){
   var db = new sqlite3.Database(file);
   db.serialize(function() {
-
   //每次先都DROP TABLE
   db.run(" DROP TABLE IF EXISTS Torrent_Info ");
-
   //db.run 如果 Staff 資料表不存在，那就建立 Staff 資料表
   db.run(
     " CREATE TABLE IF NOT EXISTS  Torrent_Info (id INTEGER PRIMARY KEY AUTOINCREMENT, article_title TEXT, article_link TEXT, torrent_link TEXT, post_time INTEGER) "
     );
-  });
+});
 
-db.close();
+  db.close();
 }
 
 function readFileToJson(path){
   var data=fs.readFileSync(path, 'utf8');
   var words=JSON.parse(data);
-  // console.log(words);
-  console.log(words.length);
-  console.log(words[0].article_title);
+  console.log('FILE ROW NUMBER: '+words.length);
+  // console.log(words[0].article_title);
   return words;
 }
 
-function write(articleInfo){
-  var db = new sqlite3.Database(file);
-  db.serialize(function() {
-    var stmt = db.prepare("INSERT INTO Torrent_Info (article_title, article_link, torrent_link, post_time)  VALUES (?,?,?,?)"); 
-    //寫進資料
-    // var temp = [
-    //   articleInfo.article_title,
-    //   articleInfo.article_link,
-    //   articleInfo.torrent_link,
-    //   articleInfo.post_time
-    //  ];
-    var temp = [
-      articleInfo,
-      articleInfo,
-      articleInfo,
-      0,
-     ];
-      stmt.run(temp);
-    stmt.finalize(); 
-  });
-  db.close();
-}
+// function write(articleInfo){
+//   var db = new sqlite3.Database(file);
+//   db.serialize(function() {
+//     var stmt = db.prepare("INSERT INTO Torrent_Info (article_title, article_link, torrent_link, post_time)  VALUES (?,?,?,?)"); 
+//     //寫進資料
+//     var temp = [
+//     articleInfo.article_title,
+//     articleInfo.article_link,
+//     articleInfo.torrent_link,
+//     articleInfo.post_time
+//     ];
+//     stmt.run(temp);
+//     stmt.finalize(); 
+//   });
+//   db.close();
+// }
 
 function writeFromFile(path){
   //讀檔
@@ -63,10 +54,10 @@ function writeFromFile(path){
     //寫進資料
     for(var i=0;i<data.length;i++){
       var temp = [
-        data[i].article_title,
-        data[i].article_link,
-        data[i].torrent_link,
-        data[i].post_time
+      data[i].article_title,
+      data[i].article_link,
+      data[i].torrent_link,
+      data[i].post_time
       ];
       stmt.run(temp);
     }
@@ -88,7 +79,7 @@ function read(){
       }
     });
   });
-db.close();
+  db.close();
 }
 // readFileToJson('./test.json');
 writeFromFile('./test.json');
